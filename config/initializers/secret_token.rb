0bdc96cb24ef83531e9c96b0460e4b46802a54b4
Kiko Penetrante <fcpenet@gmail.com>
2011-10-27 15:05:03 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TweeterKuno::Application.config.secret_token = 'bf4f601c1f1a1839f83a07cc8ecbb9df809c1943e723bdffa3d6f39623e0d5d679a5b0a11a4a9a9229ba7622aa382e47f1e72c4dba18c56ace85d1ae14a094a0'
